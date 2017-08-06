5da3484772cee2a1f4007a07e1bd4630c28a748f
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-08-06 21:44:39 +0200

    new version

--- src/main/java/fr/emse/opensensingcity/main.java
+++ src/main/java/fr/emse/opensensingcity/main.java
@@ -4,6 +4,8 @@ import fr.emse.opensensingcity.configuration.*;
 import fr.emse.opensensingcity.ldprgenerator.LDPResourceRequestGenerator;
 import fr.emse.opensensingcity.slugtemplate.IRIGenerator;
 import fr.emse.opensensingcity.tests.TestSlug;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -16,17 +18,21 @@ import java.net.URL;
  * Created by bakerally on 5/29/17.
  */
 public class main {
-    public static void main(String args []) throws IOException {
+    public static void main(String args []) throws IOException, ParseException {
         ClassLoader classLoader = main.class.getClassLoader();
         File file = new File(classLoader.getResource("Configuration.ttl").getFile());
 
-        Configuration configuration = ConfigurationFactory.createConfiguration(file.getAbsolutePath());
+        //Configuration configuration = ConfigurationFactory.createConfiguration(file.getAbsolutePath());
         //configuration.print();
-        configuration.execute();
-        LDPResourceRequestGenerator rg = new LDPResourceRequestGenerator(configuration);
+        //configuration.execute();
+        //LDPResourceRequestGenerator rg = new LDPResourceRequestGenerator(configuration);
 
 
-
+        CommandLine cl = CMDConfigurations.parseArguments(args);
+        if (cl.getOptions().length == 0){
+            CMDConfigurations.displayHelp();
+            return;
+        }
 
 
         //System.out.println("test");
